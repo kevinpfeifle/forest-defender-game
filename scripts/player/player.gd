ccdb2bfb27ef6a89947c619d55611b57f0ea62c9
Kevin Pfeifle <kevinpfeifle@outlook.com>
2024-11-17 22:30:56 -0500

    added debug label for player info

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -3,8 +3,10 @@ extends CharacterBody2D
 
 @export var animation_player: AnimationPlayer
 @export var coyote_timer: Timer
+@export var debug_label: Label
 @export var input_buffer_timer: Timer
 @export var sprite: Sprite2D
+@export var state_machine: StateMachine
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -500.0
@@ -14,6 +16,9 @@ func _process(_delta) -> void:
 		sprite.scale.x = -1
 	elif velocity.x < 0:
 		sprite.scale.x = 1
+
+	debug_label.text = "Current State: %s\nVelocity: %s" % \
+		[state_machine.current_state.state_name, velocity]
 
 func _physics_process(_delta: float) -> void:
 	move_and_slide()
